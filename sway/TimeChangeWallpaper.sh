cafccb50b0fcb012c38b03d0693967a1970d6025
fan <yfan3763@gmail.com>
2022-09-12 20:05:42 +0800

    update: update scrpt and waybar style.css
    Signed-off-by: fan <yfan3763@gmail.com>

--- sway/TimeChangeWallpaper.sh
+++ sway/TimeChangeWallpaper.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-systemctl --user import-environment SWAYSOCK
+export SWAYSOCK=$(ls /run/user/1000/sway-ipc.*.sock)
+sleep 1
 files=(/home/fan/Wallpaper/*)
+sleep 1
 swaymsg output eDP-1 bg ${files[RANDOM % ${#files[@]}]} fill
 swaymsg output HDMI-A-1 bg ${files[RANDOM % ${#files[@]}]} fill 
